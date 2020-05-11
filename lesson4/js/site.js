f7353f2ef2f9740bee861350638f97aafa7b94ca
TrentonSouth <trenton@southfamily.com>
2020-05-11 15:40:35 -0500

    Full width menu

--- lesson4/js/site.js
+++ lesson4/js/site.js
@@ -1,6 +1,12 @@
-document.getElementById("updated").innerHTML = "Last Updated " + document.lastModified;
-var date = new Date();
-document.getElementById("year").innerHTML = date.getFullYear();
+//document.getElementById("updated").innerHTML = "Last Updated " + document.lastModified;
+const date = new Date();
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+const dayName = days[date.getDay()];
+const monthName = monthNames[date.getMonth()];
+const year = date.getFullYear();
+document.getElementById("today").innerHTML = dayName + ', ' + monthName + ' ' + date.getDate() + ' ' + year;
+document.getElementById("year").innerHTML = year;
 
 function toggle_nav() {
    document.getElementById("main_nav").classList.toggle("nav_hidden");
