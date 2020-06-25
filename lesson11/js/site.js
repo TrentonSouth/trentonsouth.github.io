f25790246c6acd937c16a5184e4ed7505b5c9a59
trentonsouth <trenton.south@gmail.com>
2020-06-24 21:33:31 -0500

    Events

--- lesson11/js/site.js
+++ lesson11/js/site.js
@@ -260,3 +260,24 @@ function buildForecast(id) {
       }
    });
 }
+function getTownEvents(town) {
+   const requestURL = 'https://raw.githubusercontent.com/trentonsouth/trentonsouth.github.io/master/lesson11/data/towndata.json';
+
+fetch(requestURL)
+ .then(function (response) {
+   return response.json();
+ })
+ .then(function (jsonObject) {
+   const towns = jsonObject['towns'];
+   for (let i = 0; i < towns.length; i++ ) {
+       if (towns[i].name == town) {
+           let events = towns[i].events;
+           for (let i=0; i < events.length; i++) {
+               let event = document.createElement('p');
+               event.innerHTML = events[i];
+               document.querySelector('#events').appendChild(event);
+           }
+       }
+   }
+});
+}
