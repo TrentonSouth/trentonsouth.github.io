2986c871bc8b71369ed95a6d3d782d09d7387021
TrentonSouth <trenton@southfamily.com>
2020-06-29 11:39:04 -0500

    chart

--- scoots/js/site.js
+++ scoots/js/site.js
@@ -10,3 +10,38 @@ const monthName = monthNames[month];
 const year = date.getFullYear();
 document.getElementById("today").innerHTML = dayName + ', ' + date.getDate() + ' ' + monthName  + ' ' + year;
 document.getElementById("year").innerHTML = year;
+var data;
+loadData();
+
+
+function loadData() {
+   const requestURL = 'https://raw.githubusercontent.com/trentonsouth/trentonsouth.github.io/master/scoots/data/types.json';
+
+   fetch(requestURL)
+   .then(function (response) {
+      return response.json();
+   })
+   .then(function (jsonObject) {
+      data = jsonObject;
+   });
+}
+
+function buildChart() {
+   if (data === undefined) {
+      setTimeout(buildChart, 200);
+   } else {
+      let table = '<table id="priceChart">';
+      table += '<caption>Max Persons and Price Chart <span class="smaller">(includes Tax)</span></caption>';
+      table += '<tr><th colspan="2">&nbsp;</th><th colspan="2">Reservation</th><th colspan="2">Walk-In</th></tr>';
+      table += '<tr><th>Rental Type</th><th>Max Persons</th><th>Half Day<br>(3 hrs)</th><th>Full Day</th><th>Half Day<br>(3 hrs)</th><th>Full Day</th></tr>';
+      data.types.forEach(type => {
+         type.rentals.forEach(rental => {
+            table += '<tr><td>' + rental.rental_type + '</td><td>' + rental.max_persons + '</td><td>$' + rental.pricing.reservation.half_day;
+            table += '</td><td>$' + rental.pricing.reservation.full_day + '</td><td>$' + rental.pricing.walk_in.half_day;
+            table += '</td><td>$' + rental.pricing.walk_in.full_day + '</td></tr>'
+         })
+      });
+      table += '</table>';
+      document.getElementById('chart').innerHTML = table;
+   }
+}
