ad6acc15a57ae45446f040cb57357074047e995c
TrentonSouth <trenton@southfamily.com>
2020-06-29 11:00:24 -0500

    Scoots

---
+++ scoots/js/site.js
@@ -0,0 +1,12 @@
+const date = new Date();
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+const monthAbbrNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+const day = date.getDay();
+const dayName = days[day];
+const month = date.getMonth();
+const monthName = monthNames[month];
+const year = date.getFullYear();
+document.getElementById("today").innerHTML = dayName + ', ' + date.getDate() + ' ' + monthName  + ' ' + year;
+document.getElementById("year").innerHTML = year;
