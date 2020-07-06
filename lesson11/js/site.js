c589d7bde0b53ed36d59891fa8921d0bf976f281
TrentonSouth <trenton@southfamily.com>
2020-07-06 06:34:21 -0500

    map coords

--- lesson11/js/site.js
+++ lesson11/js/site.js
@@ -58,58 +58,26 @@ function toggle_nav() {
 }
 
 function initMap() {
-   let preston = {lat: 42.0974857, lng: -111.8788433};
-   let map = new google.maps.Map(document.getElementById('map'), {zoom: 13, center: preston});
+   
+   parts = window.location.pathname.split('/');
+   page = parts[parts.length - 1];
+   let coords = "";
+   switch (page) {
+      case 'preston.html':
+         coords = {lat: 42.0974857, lng: -111.8788433};
+      break;
+      case 'sodasprings.html':
+         coords = {lat: 42.6544, lng: -111.6047};
+      break;
+      case 'fishhaven.html':
+         coords = {lat: 42.0372, lng: -111.3960};
+      break;
+   }
+   
+   let map = new google.maps.Map(document.getElementById('map'), {zoom: 13, center: coords});
 }
 
-/*function buildForecast() {
-   let daysInMonth = new Date(year, month + 1, 0).getDate();
-   let dom = date.getDate();
-   let day1, day2, day3, day4, day5, abbrMonth1, abbrMonth2, abbrMonth3, abbrMonth4, abbrMonth5;
-   if (dom + 1 <= daysInMonth) {
-      day1 = dom + 1;
-      abbrMonth1 =  monthAbbrNames[month]
-   } else {
-      day1 = dom + 1 - daysInMonth;
-      abbrMonth1 = month < 11 ? monthAbbrNames[month+1] : monthAbbrNames[0];
-   }
-   if (dom + 2 <= daysInMonth) {
-      day2 = dom + 2;
-      abbrMonth2 =  monthAbbrNames[month]
-   } else {
-      day2 = dom + 2 - daysInMonth;
-      abbrMonth2 = month < 11 ? monthAbbrNames[month+1] : monthAbbrNames[0];
-   }
-   if (dom + 3 <= daysInMonth) {
-      day3 = dom + 3;
-      abbrMonth3 =  monthAbbrNames[month]
-   } else {
-      day3 = dom + 3 - daysInMonth;
-      abbrMonth3 = month < 11 ? monthAbbrNames[month+1] : monthAbbrNames[0];
-   }
-   if (dom + 4 <= daysInMonth) {
-      day4 = dom + 4;
-      abbrMonth4 =  monthAbbrNames[month]
-   } else {
-      day4= dom + 4 - daysInMonth;
-      abbrMonth4 = month < 11 ? monthAbbrNames[month+1] : monthAbbrNames[0];
-   }
-   if (dom + 5 <= daysInMonth) {
-      day5 = dom + 5;
-      abbrMonth5 =  monthAbbrNames[month]
-   } else {
-      day5 = dom + 5 - daysInMonth;
-      abbrMonth5 = month < 11 ? monthAbbrNames[month+1] : monthAbbrNames[0];
-   }
 
-   document.getElementById('forecast1').innerHTML = buildForecastDay(day1, abbrMonth1, 'cloudy.jpg', 'Cloudy', 27);
-   document.getElementById('forecast2').innerHTML = buildForecastDay(day2, abbrMonth2, 'rain.jpg', 'Rainy', 34);
-   document.getElementById('forecast3').innerHTML = buildForecastDay(day3, abbrMonth3, 'sunshine.jpg', 'Sunny', 30);
-   document.getElementById('forecast4').innerHTML = buildForecastDay(day4, abbrMonth4, 'storm.jpg', 'Stormy', 31);
-   document.getElementById('forecast5').innerHTML = buildForecastDay(day5, abbrMonth5, 'snow.jpg', 'Snowy', 26);
-
-}
-*/
 
 function buildForecastDay(dayOfMonth, abbrMonth, image, desc, temp) {
    html = '<h3>' + abbrMonth + ' ' + dayOfMonth + '</h3>';
